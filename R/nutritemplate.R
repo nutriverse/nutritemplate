08a9f558a3424b301bc4cc7b8aa490dc06f0a84d
Ernest Guevarra <ernest@guevarra.io>
2024-12-28 18:34:52 +0000

    initial commit

---
+++ R/nutritemplate.R
@@ -0,0 +1,9 @@
+#'
+#' Template and Utilities for nutriverse Package Documentation
+#' 
+#' A private template for use by nutriverse packages.
+#'
+#' @keywords internal
+#' @docType package
+#' @name nutritemplate
+#'
---
+++ R/testing.R
@@ -0,0 +1,32 @@
+#' Test function 1
+#' 
+#' @examples
+#' test1()
+#' 
+#' @export
+#' 
+test1 <- function() {
+  print("Hello")
+}
+
+#' Test function 2
+#' 
+#' @examples
+#' test2()
+#' 
+#' @export
+#' 
+test2<- function() {
+  print("Hello")
+}
+
+#' Test function 3
+#' 
+#' @examples
+#' test3()
+#' 
+#' @export
+#' 
+test3 <- function() {
+  print("Hello")
+}
